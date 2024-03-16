a73069c31ad175fb5ed74a8024e2bbf1bef241b0
sukirti kumar jha <sukritkumar2002@gmail.com>
2024-03-17 01:57:31 +0530

    initial commit

---
+++ readme.md
@@ -0,0 +1,26 @@
+# Weather App
+
+This is a simple weather application that allows users to view the current weather details for a specific city. Users can also change the city dynamically to view weather information for different locations.
+
+## Tech Stack
+
+- HTML
+- CSS
+- JavaScript
+- Node.js
+- Axios
+
+## Features
+
+- Display current weather details such as temperature, wind speed, and time.
+- Dynamically change the city to view weather information for different locations.
+- Visual elements such as sun, clouds, and wind effects for a more engaging user experience.
+
+## Installation
+
+To run the Weather App locally on your machine, follow these steps:
+
+1. Clone this repository to your local machine:
+
+   ```bash
+   git clone <repository-url>
---
+++ script.js
@@ -0,0 +1,42 @@
+const http = require("http");
+const fs = require("fs");
+const axios  = require("axios");
+
+const homeFile = fs.readFileSync("index.html","UTF-8");
+console.log("successfully loaded")
+
+const replaceData = (oldVal,newVal) => { 
+    let temperature = oldVal.replace("{%location%}",newVal.location.name);
+    temperature = temperature.replace("{%temperature%}",newVal.current.temp_c);
+    temperature = temperature.replace("{%wind%}",newVal.current.wind_kph);
+    temperature = temperature.replace("{%Time%}",newVal.location.localtime);
+    return temperature;
+}
+const apiKey = "9fa3194201774b0e9ff200825241503";
+const city = "bangalore";
+
+
+const serverStart = http.createServer((req,res)=>{
+    if(req.url == '/'){
+        axios.get('http://api.weatherapi.com/v1/current.json?key='+apiKey+'&q='+city)
+        .then(function (response){
+            let arrData = [response.data];
+            let realD = arrData.map((val)=>replaceData(homeFile,val));
+            let concetratedData = realD.join(" ");
+            res.writeHead(200,{'Content-Type' : 'html'});
+            res.write(concetratedData);
+            res.end();
+            console.log(response.data);
+        })
+        .catch(function (err){
+            console.log(err);
+            res.end("file not found")
+        })
+        .finally(function (){
+            console.log("working fine")
+        })
+    }
+})
+
+serverStart.listen(8080,"127.0.0.1");
+
