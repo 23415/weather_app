6ef113333b5bce941b4119e9aeb5dd78810ccd79
sukirti kumar jha <sukritkumar2002@gmail.com>
2024-03-17 23:46:48 +0530

    changed name

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const serverStart = http.createServer((req,res)=>{
             let realD = arrData.map((val)=>replaceData(homeFile,val));
             let concetratedData = realD.join(" ");
             res.writeHead(200,{'Content-Type' : 'html'});
-            fs.writeFileSync('rendered.html',concetratedData);
+            fs.writeFileSync('index.html',concetratedData);
             res.write(concetratedData);
             res.end();
             console.log(response.data);
